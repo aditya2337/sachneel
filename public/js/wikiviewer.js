d17a7f6795ae18f416cd7ba16de4320e6b237c3c
aditya2337 <aditya.n@gai.co.in>
2017-03-05 11:28:19 +0530

    finished and pushing up to git

--- public/js/wikiviewer.js
+++ public/js/wikiviewer.js
@@ -1,7 +1,26 @@
 // https://www.medizawiki.org/wiki/API:Main_page
-var empty;
-fetch('https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=earth')
-  .then((res) => {
-    empty = res;
+var searchText;
+function getResults () {
+  searchText = document.getElementById('search-text').value;
+  var el = document.getElementById('search-results');
+  a().then(res => {
+    // console.log(res.query.search[0].title);
+    res.query.search.map(val => {
+      var h1 = document.createElement('H1');
+      var p = document.createElement('P');
+      var title = document.createTextNode(val.title);
+      var snippet = document.createTextNode(val.snippet);
+      h1.appendChild(title);
+      p.appendChild(snippet);
+      el.appendChild(h1);
+      el.appendChild(p);
+      // console.log(val.title, val.snippet);
+    });
   });
-console.log(empty);
+}
+
+function a () {
+  return fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=${searchText}`)
+    .then(storage => storage.json())
+  ;
+}
